7018a2fc0cfc203ccf44a8f4ed910e01d33ebf0e
codeNinja-1 <74162951+codeNinja-1@users.noreply.github.com>
2021-03-18 11:57:19 -0700

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,7 @@
 
 # Set up server
 
+import time
 from aiohttp import web
 import socketio
 
@@ -41,29 +42,55 @@ async def routing(request):
 
 # A list of people on the list
 people = []
-pw = "FuzzyWuzzy!"
+pw = "a"
+lastfail = 0
 
 @sio.on('to_server')
 async def event_to_server(sid, data):
+	global lastfail
+	global pw
+	global people
+	if data['type'] == 'get':
+		await sio.emit('to_client', {'type':'people', 'people':people}, room=sid)
+		return
+	if not (lastfail < time.time() - 5):
+		await sio.emit('to_client', {'type':'wait'}, room=sid)
+		return
 	if data['type'] == 'verify':
 		EnteredPW = (data['pw'])
 		if EnteredPW == pw:
 			await sio.emit('to_client', {'type':'verify', 'result':True}, room=sid)
 		else:
+			lastfail = time.time()
 			await sio.emit('to_client', {'type':'verify', 'result':False}, room=sid)
 		return
-	EnteredPW = (data['pw'])
-	if EnteredPW == pw:
+	if data['pw'] == pw:
 		# The password is correct
 		if data['type'] == 'add':
-			# User requested to add a person
-			people.append(data['name'])
+			found = False
+			for iteration in range(len(people)):
+				if str(people[iteration]) == str(data['name']):
+					found = True
+			if not found: #checks if the person was already added to the list
+				#The person is not in the list
+				people.append(data['name'])
+			else:
+				await sio.emit('to_client', {'type':'in_list', 'name':data['name']}, room=sid)
 		elif data['type'] == 'remove':
 			# User requested to remove a person.
-			people.remove(str(data['name']))
+			found = False
+			for iteration in range(len(people)):
+				if str(people[iteration]) == str(data['name']):
+					found = True
+			if found:
+				people.remove(str(data['name']))
+			else:
+				await sio.emit('to_client', {'type':'not_found', 'name':data['name']}, room=sid)
 		# Every time an event is received, broadcast the list
 		# of people to all clients
 		await sio.emit('to_client', {'type':'people', 'people':people})
+	else:
+		lastfail = time.time()
 
 
 # Attach routing function to server get request for "/"
